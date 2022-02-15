b5e90a43c2afa8841e8369d4b8590af44869ce87
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2022-02-15 10:54:26 +0530

    Noticeboard list

--- devgree/user/urls.py
+++ devgree/user/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', ContactUsCreateView.as_view(), name='contact'),
+    path('noticeboard/', NoticeboardListView.as_view(), name='noticeboard'),
+    path('noticeboard/<int:id>/', NoticeboardDetailView.as_view(), name='noticeboard_detail'),
 ]
--- devgree/user/views.py
+++ devgree/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from administrator.forms import *
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -19,3 +19,13 @@ class ContactUsCreateView(SuccessMessageMixin, CreateView):
     template_name = 'user/contact.html'
     success_url = reverse_lazy('contact')
     success_message = 'Your message has been sent successfully.'
+
+class NoticeboardListView(ListView):
+    model = Noticeboard
+    template_name = 'user/noticeboard/list.html'
+    queryset = Noticeboard.objects.filter(department__isnull=True)
+
+class NoticeboardDetailView(DetailView):
+    model = Noticeboard
+    template_name = 'user/noticeboard/detail.html'
+    pk_url_kwarg = 'id'
