75049d818b534c6d1d64cf312b813ded37337706
JesherBernett <jesherbernett2001@gmail.com>
2022-04-04 00:32:52 +0530

    minor changes

--- devgree/devgree/settings.py
+++ devgree/devgree/settings.py
@@ -89,6 +89,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'devgree',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
